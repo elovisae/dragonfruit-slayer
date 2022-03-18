466a0170fa7f0e7471f8c8f1fbcca760f147e029
Lovisa Eriksson <lovisa@ahna.se>
2022-03-18 18:42:33 +0100

    init cart done

--- dragonfruit-slayer/src/Components/shopping-cart/ShoppingCart.jsx
+++ dragonfruit-slayer/src/Components/shopping-cart/ShoppingCart.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './shoppingCart.css'
+import { useNavigate } from "react-router-dom";
 
 const ShoppingCart = () => {
+    const navigate = useNavigate();
   return (
-    <h1>ShoppingCart</h1>
+     <div id="wrapper">
+         <nav className="nav">
+            <p onClick={() => {navigate(-1)}}> &lt; Back</p>
+            <h1 onClick={() => {navigate('/')}}>SLAYER</h1>
+         </nav>
+         <h2>Kassa</h2>
+         <article className='cart'>
+             <section>
+                <h3>Din varukorg:</h3>
+                <div>
+
+                </div>
+             </section>
+             <section>
+                 <h3>Till betalning:</h3>
+             </section>
+         </article>
+         <footer>
+            <div>Contact us</div>
+            <div>Contact us</div>
+            <div>Contact us</div>
+         </footer>
+     </div>
   )
 }
 
