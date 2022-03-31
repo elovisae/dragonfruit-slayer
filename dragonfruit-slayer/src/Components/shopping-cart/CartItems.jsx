b28c19bfe5815b5c3f56f3854ec5b93e433d9f13
Lovisa Eriksson <lovisa@ahna.se>
2022-03-31 14:37:51 +0200

    shopping cart done

---
+++ dragonfruit-slayer/src/Components/shopping-cart/CartItems.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import CartItem from './CartItem'
+
+const CartItems = (props) => {
+    const items = props.items;
+  return (
+    <>
+    {items.map((item) => 
+        <CartItem 
+        item={item}
+        key={item._id}
+        
+        />
+    )}
+    </>
+  )
+}
+
+export default CartItems
