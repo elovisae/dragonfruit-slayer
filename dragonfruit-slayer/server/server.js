3c42781332a9c9e58a7819389a28222cb38f5302
Lovisa Eriksson <lovisa@ahna.se>
2022-03-31 10:20:52 +0200

    Items fetch done

--- dragonfruit-slayer/server/server.js
+++ dragonfruit-slayer/server/server.js
@@ -4,7 +4,6 @@ const bodyParser    = require('body-parser')
 const cors          = require('cors')
 const Item          = require('./modules/Item')
 const User          = require('./modules/User')
-const { getMaxListeners } = require('./modules/Item')
 
 const app           = express()
 app.use(cors())
